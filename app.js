a49786267112aa4607142e5091c35a72ac35246a
Sk1er <sk1er@sk1er.club>
2019-01-30 12:46:04 -0500

    Admin stuffs

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var submit = require('./routes/submit');
 let create = require('./routes/create');
+let admin = require('./routes/admin');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -28,6 +30,7 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/submit', submit);
 app.use("/create", create);
+app.use("/admin", admin);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var database = require(".././database/database");
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+    let mealsState = await database.getMealsState();
+    const db_max_meals_stay = mealsState['max_meals_stay'];
+    const db_max_meals_go = mealsState['max_meals_stay'];
+
+
+    console.log(mealsState);
+    res.render('admin', {title: 'Caritas',
+        meals_stay: db_max_meals_stay,
+        meals_go: db_max_meals_go});
+});
+
+module.exports = router;
