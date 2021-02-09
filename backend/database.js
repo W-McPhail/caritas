5b5701dad4c1d205a80e5375324122daded68c31
w-mcphail <william.m.mcphail@gmail.com>
2021-02-08 20:00:46 -0500

    mongodb - hosted cluster on AWS

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://db:<pass>{}@mongodbc0.vvmio.mongodb.net/admin?authSource=admin&replicaSet=atlas-4dd97d-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+var url = "mongodb+srv://db:flipAcoin22{}@mongodbc0.vvmio.mongodb.net/admin?authSource=admin&replicaSet=atlas-4dd97d-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 console.log("Starting db");
 var database;
 MongoClient.connect(url, async function (err, db) {
